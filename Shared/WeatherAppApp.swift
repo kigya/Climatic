dddb900e68d9ab29e3edf740fee80cb2f611ea9a
kirillborichevskiy <kirilborichevskiy@gmail.com>
2022-03-09 14:13:31 +0300

    Initial commit

---
+++ Shared/WeatherAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherAppApp.swift
+//  Shared
+//
+//  Created by Kirill Borichevskiy on 9.03.22.
+//
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
