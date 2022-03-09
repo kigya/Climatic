c90493912f203a0fe1daa4979356315cc2f9aef2
kirillborichevskiy <kirilborichevskiy@gmail.com>
2022-03-09 16:45:32 +0300

    Add visual functionality

---
+++ Shared/WeatherButton.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherButton.swift
+//  WeatherApp
+//
+//  Created by Kirill Borichevskiy on 9.03.22.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+
+    var body: some View {
+        Button {
+            print("tapped")
+        } label: {
+            Text(title)
+                    .frame(width: 280, height: 50)
+                    .background(backgroundColor)
+                    .foregroundColor(textColor)
+                    .font(.system(size: 20, weight: .bold, design: .default))
+                    .cornerRadius(10)
+        }
+    }
+}
